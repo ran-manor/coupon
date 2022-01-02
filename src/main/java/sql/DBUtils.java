149473708a92bb9cb5bdf326ff93a4b66346dc86
alonmintz <alonmintz@gmail.com>
2022-01-02 13:21:20 +0200

    alon

--- src/main/java/sql/DBUtils.java
+++ src/main/java/sql/DBUtils.java
@@ -1,8 +1,10 @@
 package sql;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class DBUtils {
     public static void runQuery(String query){
@@ -21,5 +23,37 @@ public class DBUtils {
             ConnectionPool.getInstance().returnConnection(connection);
         }
 
+    }
+
+    public static void runQuery(String query, Map<Integer, Object> params) throws SQLException {
+        try {
+            //connection = ConnectionPool.getInstance().getConneciton();
+            PreparedStatement statement = connection.prepareStatement(query);
+            params.forEach((key, value) -> {
+                try {
+                    if (value instanceof Integer) {
+                        statement.setInt(key, (Integer) value);
+                    } else if (value instanceof String) {
+                        statement.setString(key, String.valueOf(value));
+                    } else if (value instanceof Date) {
+                        statement.setDate(key, (Date) value);
+                    } else if (value instanceof Double) {
+                        statement.setDouble(key, (Double) value);
+                    } else if (value instanceof Boolean){
+                        statement.setBoolean(key,(Boolean) value);
+                    } else if (value instanceof Float){
+                        statement.setFloat(key,(Float)value);
+                    }
+                } catch (SQLException err) {
+                    System.out.println(err.getMessage());
+                }
+
+            });
+            statement.execute();
+        } catch (SQLException err) {
+            System.out.println(err.getMessage());
+        } finally {
+            //ConnectionPool.getInstance().returnConnection(connection)
+        }
     }
 }
