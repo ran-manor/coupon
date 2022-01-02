1892d95edb4c39eca08d0892a04c5300e1ba32fd
ran-manor <raran937@gmail.com>
2022-01-02 13:43:57 +0200

    2/1/2022 stage 1

--- src/main/java/DBDAO/CustomersDBDAO.java
+++ src/main/java/DBDAO/CustomersDBDAO.java
@@ -2,17 +2,55 @@ package DBDAO;
 
 import Beans.Customer;
 import DAO.CustomerDAO;
+import sql.ConnectionPool;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomersDBDAO implements CustomerDAO {
+    private final String ADD_CUSTOMER="INSERT INTO `couponmania`.`customers` " +
+            "(`first_name`,`last_name`,`email`,`password`)" +
+            "VALUES (?,?,?,?);";
+
+    private final String UPDATE_CUSTOMER = "UPDATE `couponmania`.`customers` " +
+            "SET first_name=?, last_name=?, email=? ,password=?" +
+            "WHERE id=?";
+
+    private final String DELETE_CUSTOMER = "DELETE FROM `couponmania`.`customers` WHERE id=?";
+
+    private final String GET_ALL_CUSTOMERS = "SELECT * FROM `couponmania`.`customers`";
+
+    private final String GET_ONE_CUSTOMER_BY_ID = "SELECT * FROM `couponmania`.`customers` WHERE id LIKE ?";
+    private Connection connection;
+    private boolean isOK;
+
     @Override
     public boolean isCustomerExists(String email, String password) {
         return false;
     }
 
     @Override
-    public void addCustomer(Customer customer) {
+    public boolean addCustomer(Customer customer) {
+        Map<Integer,Object> parmas = new HashMap<>();
+        try {
+            connection = ConnectionPool.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(ADD_CUSTOMER);
+            parmas.put(1,customer.getFirstName());
+            parmas.put(2,customer.getLastName());
+            parmas.put(3,customer.getEmail());
+            parmas.put(4,customer.getPassword());
+        } catch (InterruptedException | SQLException err) {
+            System.out.printf(err.getMessage());
+            isOK = false;
+        }
+        finally {
+            ConnectionPool.getInstance().returnConnection(connection);
+        }
+        return isOK;
 
     }
 
