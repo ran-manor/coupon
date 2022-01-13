5d8af70162114351b667f979e4ddde06abe1616d
ran-manor <raran937@gmail.com>
2022-01-13 12:14:32 +0200

    12/1/2022 CustomerFacade Exceptions

--- src/main/java/Facade/CustomerFacade.java
+++ src/main/java/Facade/CustomerFacade.java
@@ -23,26 +23,28 @@ public class CustomerFacade extends ClientFacade {
     }
 
 
-    public void purchaseCoupon(Coupon coupon) throws Exception {
+    public void purchaseCoupon(Coupon coupon) {
         Customer customer = customerDAO.getOneCustomer((int) customerId);
-        if (coupon.getAmount() == 0) {
-            throw new CouponSystemExceptions(CustomerErrorMsg.AMOUNT_EQUAL_ZERO);
-        } else if (coupon.getEndDate().after(new Date())) {
-            throw new CouponSystemExceptions(CustomerErrorMsg.EXPIRED_DATE);
-        } else if (couponDAO.getAllCouponPurchases().get(customerId).contains(coupon.getId())) {
-            throw new CouponSystemExceptions(CustomerErrorMsg.COUPON_ALREADY_EXISTS);
+        try {
+            if (coupon.getAmount() == 0) {
+                throw new CouponSystemExceptions(CustomerErrorMsg.AMOUNT_EQUAL_ZERO);
+            } else if (coupon.getEndDate().after(new Date())) {
+                throw new CouponSystemExceptions(CustomerErrorMsg.EXPIRED_DATE);
+            } else if (couponDAO.getAllCouponPurchases().get(customerId).contains(coupon.getId())) {
+                throw new CouponSystemExceptions(CustomerErrorMsg.COUPON_ALREADY_EXISTS);
+            }
+            //gets here if the purchase answers all conditions
+            else {
+                couponDAO.addCouponPurchase(customerId, coupon.getId());
+
+                //Decrease coupon amount by 1
+                Coupon c = couponDAO.getOneCoupon(coupon.getId());
+                c.setAmount(c.getAmount() - 1);
+                couponDAO.updateCoupon(c);
+            }
+        } catch (CouponSystemExceptions err) {
+            System.out.println(err.getMessage());
         }
-        //gets here if the purchase answers all conditions
-        else {
-            couponDAO.addCouponPurchase(customerId, coupon.getId());
-
-            //Decrease coupon amount by 1
-            Coupon c = couponDAO.getOneCoupon(coupon.getId());
-            c.setAmount(c.getAmount() - 1);
-            couponDAO.updateCoupon(c);
-        }
-
-
     }
 
     public ArrayList<Coupon> getCustomersCoupons() {
@@ -84,10 +86,15 @@ public class CustomerFacade extends ClientFacade {
         return ownedCoupons;
     }
 
-    public Customer getCustomerDetails() throws Exception{
+    public Customer getCustomerDetails() {
         Customer customer = customerDAO.getOneCustomer(customerId);
-        if(customer == null){
-            throw new CouponSystemExceptions(CustomerErrorMsg.CUSTOMER_NOT_EXIST);
+        try {
+            if (customer == null) {
+                throw new CouponSystemExceptions(CustomerErrorMsg.CUSTOMER_NOT_EXIST);
+            }
+        } catch (CouponSystemExceptions err) {
+            System.out.println(err.getMessage());
+
         }
         return customer;
     }
