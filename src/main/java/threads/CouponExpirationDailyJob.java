f8e599f1da68c506a2b118ea964a9eb3a8966402
alonmintz <alonmintz@gmail.com>
2022-01-13 13:36:04 +0200

    13/01/2022 created thread

---
+++ src/main/java/threads/CouponExpirationDailyJob.java
@@ -0,0 +1,41 @@
+package threads;
+
+import Beans.Coupon;
+import DAO.CouponDAO;
+import DBDAO.CouponsDBDAO;
+import utils.DateUtils;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CouponExpirationDailyJob implements Runnable {
+
+    private final CouponDAO couponDAO;
+    private boolean quit;
+
+    public CouponExpirationDailyJob(CouponDAO couponDAO, boolean quit) {
+        this.couponDAO = new CouponsDBDAO();
+        this.quit = false;
+    }
+
+    @Override
+    public void run() {
+        while (!quit){
+            couponDAO.getAllCoupons().stream()
+                    .filter(coupon-> coupon.getEndDate().after(DateUtils.localDateToSqlDate(LocalDate.now())))
+                    .forEach(coupon -> couponDAO.deleteCoupon(coupon.getId()));
+            try {
+                Thread.sleep(1000*60*60*24);
+            } catch (InterruptedException err) {
+                System.out.println(err.getMessage());
+                stop();
+            }
+        }
+    }
+
+    public void stop() {
+        this.quit = true;
+    }
+}
