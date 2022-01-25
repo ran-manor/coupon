4f44ca2ea4ff3ef02c6c21fcc28829b2c49d290c
tamboor <nirkatz20exc@gmail.com>
2022-01-25 11:39:20 +0200

    added todo in loginexception

--- src/main/java/exceptions/LoginException.java
+++ src/main/java/exceptions/LoginException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class LoginException extends  Exception{
     public LoginException(){super("Error");}
-
+    //TODO: Should we delete this (use on eerror type)
     public LoginException(String message){super(message);}
 }
